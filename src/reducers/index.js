9060f4f3887fcce1c582079c5775cc39c23f7e6d
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-09-02 10:21:13 +0100

    adds a reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,60 @@
+import {addRecipe, removeFromCalendar } from '../actions'
+
+const initialCalendarState = {
+    sunday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    monday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    tuesday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    wednesday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    thursday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    friday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    saturday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+}
+
+function calendar (state= initialCalendarState, action) {
+  const { day, recipe, meal } = action // here picking off the action's payload
+
+  switch(action.type) {
+    case ADD_RECIPE:
+      return { ...state, 
+                [day]:{ ...state[day],[meal]:recipe.label}
+              }
+    case REMOVE_FROM_CALENDAR:
+      return { ...state,
+                [day]:{ ...state[day],[meal]:null}
+              }
+    default:
+    return state
+  }
+}
+
+export default calender
+
+
