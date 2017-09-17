963cc92b95b92a2aefa7f39a721f11145da01239
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-09-17 02:23:17 +0100

    uses combineReducers to combine reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import { addRecipe, removeFromCalendar } from '../actions'
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
+    console.log('Props', this.props)
     return (
       <div>
         Hello World!!
@@ -10,4 +12,29 @@ class App extends Component {
     )
   }
 }
-export default App
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data)=>dispatch(addRecipe(data)),
+    remove: (data)=>dispatch(removeFromCalendar(data))
+  }
+}
+
+function mapStateToProps({ calendar, food }){
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+  return {
+    calendar: dayOrder.map(day=>(
+      {
+        day: day,
+        meals:Object.keys(calendar[day]).reduce((meals, meal)=>{
+          meals[meal]=calendar[day][meal]
+            ?food[calendar[day][meal]]
+            :null
+          return meals
+        },{})
+
+      }
+    ))
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
